4f87f0fa819daa140eb5e2c976fb1d3744178917
lijian <1976662461@qq.com>
2020-01-10 11:28:51 +0800

    [add] 创立数据库连接类

---
+++ DaoNet/DaoMvc/DAL/SqlConnectionGetStr.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class SqlConnectionGetStr
+    {
+       static SqlConnection conn = null;
+       static string sqlStr = "Data Source=LAPTOP-5IME6759/SQLEXPRESS;Initial Catalog=User;User ID=sa;Password=123";
+        public static void getSqlConnectionStr()
+        {
+            conn = new SqlConnection(sqlStr);
+            conn.Open();
+        }
+
+        public static void EndSqlConnectionStr()
+        {
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ DaoNet/DaoMvc/Model/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 顾客类
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address { get; set; }
+    }
+}
